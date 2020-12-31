2d24a1ece724e83499ea2a2da7abbdd484c13038
lampperson <jamesevora367@gmail.com>
2020-12-30 18:50:54 -0800

    commit_12/30/2020
    data

--- Singletons/Globals.gd
+++ Singletons/Globals.gd
@@ -4,5 +4,25 @@ const GRAVITY = 52
 
 var paused = false
 
+var data = {}
+
 func _ready():
 	ProjectSettings.set("physics/3d/default_gravity", GRAVITY)
+	
+	# Loading in data
+	var data_read = File.new()
+	data_read.open("res://data.cdb", File.READ)
+	var data_json = parse_json(data_read.get_as_text())
+	data_read.close()
+	
+	for sheet in data_json["sheets"]:
+		var lines = sheet["lines"].duplicate(true)
+		var items = {}
+		for line in lines:
+			var id = line["ID"]
+			line.erase("ID")
+			items[id] = line
+		
+		data[sheet["name"]] = items
+	
+	print(data["Weapons"]["StartingPistol"]["Damage"])
